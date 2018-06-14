c5a9c1544efde068579074bd8c2024d1fc461b43
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-14 12:12:18 +0500

    Задание выполнено

---
+++ Practice__9/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InputOutputLib;
+
+namespace Practice__9
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            El List = El.MakeList(Get.Int32("Введите количество элементов в списке: "));
+            List.Print();
+            List.Remove(Get.Int32("Введите удаляемый элемент: "));
+            List.Print();
+            Console.ReadKey();
+        }
+    }
+}
